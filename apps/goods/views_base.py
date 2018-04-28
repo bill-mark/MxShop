98c426cf9a50cbc6c054d437a6d7bd5b14f928fb
zxdeng <deng.zx@belle.com.cn>
2018-04-28 15:22:52 +0800

    up to

--- apps/goods/views_base.py
+++ apps/goods/views_base.py
@@ -2,3 +2,24 @@
 
 
 from django.views.generic.base import View
+
+from goods.models import Goods
+
+
+
+class GoodsListView(View):
+    def get(self,request):
+        "通过django的view实现商品列表页"
+
+        json_list = []
+        goods = Goods.objects.all()[0:10] #切片操作 取前十个数
+        for good in goods:
+            json_dict = {}
+            json_dict["name"] = good.name
+            json_dict["category"] = good.category.name
+            json_dict["market_price"] = good.market_price
+            json_list.append(json_dict)
+
+        from django.http import HttpResponse
+        import json
+        return HttpResponse(json.dumps(json_list),content_type="application/json")
