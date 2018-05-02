fa0c21bd490477018739e5da29a53fd6fb10cb92
bill.mark <weiguicheng@foxmail.com>
2018-05-02 10:42:13 +0800

    up

--- apps/goods/views_base.py
+++ apps/goods/views_base.py
@@ -13,15 +13,21 @@ class GoodsListView(View):
 
         json_list = []
         goods = Goods.objects.all()[0:10] #切片操作 取前十个数 0可以省略
-        for good in goods:
-            json_dict = {}
-            json_dict["name"] = good.name
-            json_dict["category"] = good.category.name
-            json_dict["market_price"] = good.market_price
-            json_list.append(json_dict)
+        # for good in goods:
+        #     json_dict = {}
+        #     json_dict["name"] = good.name
+        #     json_dict["category"] = good.category.name
+        #     json_dict["market_price"] = good.market_price
+        #     json_list.append(json_dict)
 
-        from django.forms.models import model_to_dict
+        # from django.forms.models import model_to_dict
+        # for good in goods:
+        #     json_dict = model_to_dict(good)
+        #     json_list.append(json_dict)
 
-        from django.http import HttpResponse
         import json
-        return HttpResponse(json.dumps(json_list),content_type="application/json")
+        from django.core import serializers
+        json_data = serializers.serialize("json",goods)
+        json_data = json.loads(json_data)
+        from django.http import JsonResponse
+        return JsonResponse(json_data,safe=False)
